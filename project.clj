beded99148208f85016cae71100ded1b70dd855c
Maksym Prokopov <mprokopov@gmail.com>
2021-06-13 15:26:15 +0200

    bump dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject clj-btc "0.11.2"
+(defproject clj-btc "0.12.2-SNAPSHOT"
   :description "Clojure bindings for the original (C++) Bitcoin Client"
   :url "https://github.com/aviad/clj-btc"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/data.json "0.2.6"]
-                 [http-kit "2.1.19"]])
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/data.json "2.3.1"]
+                 [http-kit "2.5.3"]])
